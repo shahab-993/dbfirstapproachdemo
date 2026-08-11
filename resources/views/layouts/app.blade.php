99843dbd794375bc43f9a5dd66b377e566e8d37c
shahab-993 <ahmad.k.shahab@gmail.com>
2026-08-11 19:17:08 +0430

    get data from mysql procedur

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet" />
+    <title>Advanced Laravel Training Demos</title>
+</head>
+
+<body>
+    <div>
+        @yield('content')
+    </div>
+</body>
+
+</html>
